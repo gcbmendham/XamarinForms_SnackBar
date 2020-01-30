2cbec832c19f1a4391cc25d580ef35dbbc11824d
Gary Butcher <gcbmendham@gmail.com>
2020-01-30 14:05:54 +0000

    refinements to SnackBar control

--- Xamarin.FormsSnackBarDemo/Xamarin.FormsSnackBarDemo/SnackBar.xaml.cs
+++ Xamarin.FormsSnackBarDemo/Xamarin.FormsSnackBarDemo/SnackBar.xaml.cs
@@ -13,58 +13,73 @@ namespace Xamarin.FormsSnackBarDemo
 
         public SnackBar()
         {
-            this.TranslateTo(0, 50, 0);  // ensure the snack bar is initially off-screen
-            _timer = new Timer(CloseOnTimeout);
             InitializeComponent();
+
+            _timer = new Timer(CloseOnTimeout);
+            this.TranslateTo(0, HeightRequest, 0);  // ensure the snack bar is initially off-screen
         }
 
         #region Bindable Properties
-        public static readonly BindableProperty ButtonTextColorProperty = BindableProperty.Create("ButtonTextColor", typeof(Color), typeof(SnackBar), default(Color));
+
+        public new static readonly BindableProperty HeightRequestProperty = BindableProperty.Create(nameof(HeightRequest), typeof(int), typeof(SnackBar), 50, propertyChanged: OnHeightRequestChanged);
+
+        private static void OnHeightRequestChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            (bindable as SnackBar).TranslateTo(0, (int)newValue, 0);
+        }
+
+        public new int HeightRequest
+        {
+            get => (int)GetValue(HeightRequestProperty);
+            set { SetValue(HeightRequestProperty, value); }
+        }
+
+        public static readonly BindableProperty ButtonTextColorProperty = BindableProperty.Create(nameof(ButtonTextColor), typeof(Color), typeof(SnackBar), default(Color));
         public Color ButtonTextColor
         {
-            get { return (Color)GetValue(ButtonTextColorProperty); }
+            get => (Color)GetValue(ButtonTextColorProperty);
             set { SetValue(ButtonTextColorProperty, value); }
         }
 
-        public static readonly BindableProperty MessageProperty = BindableProperty.Create("Message", typeof(string), typeof(SnackBar), default(string));
+        public static readonly BindableProperty MessageProperty = BindableProperty.Create(nameof(Message), typeof(string), typeof(SnackBar), default(string));
         public string Message
         {
-            get { return (string)GetValue(MessageProperty); }
+            get => (string)GetValue(MessageProperty);
             set { SetValue(MessageProperty, value); }
         }
 
-        public static readonly BindableProperty CloseButtonTextProperty = BindableProperty.Create("CloseButtonText", typeof(string), typeof(SnackBar), "Close");
-        public string CloseButtonText
+        public static readonly BindableProperty ButtonTextProperty = BindableProperty.Create(nameof(ButtonText), typeof(string), typeof(SnackBar), "Close");
+        public string ButtonText
         {
-            get { return (string)GetValue(CloseButtonTextProperty); }
-            set { SetValue(CloseButtonTextProperty, value); }
+            get =>  (string)GetValue(ButtonTextProperty); 
+            set { SetValue(ButtonTextProperty, value); }
         }
 
-        public static readonly BindableProperty FontSizeProperty = BindableProperty.Create("FontSize", typeof(float), typeof(SnackBar), default(float));
+        public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(float), typeof(SnackBar), default(float));
         public float FontSize
         {
-            get { return (float)GetValue(FontSizeProperty); }
+            get =>  (float)GetValue(FontSizeProperty); 
             set { SetValue(FontSizeProperty, value); }
         }
 
-        public static readonly BindableProperty TextColorProperty = BindableProperty.Create("TextColor", typeof(Color), typeof(SnackBar), Color.White);
+        public static readonly BindableProperty TextColorProperty = BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(SnackBar), Color.White);
         public Color TextColor
         {
-            get { return (Color)GetValue(TextColorProperty); }
+            get =>  (Color)GetValue(TextColorProperty); 
             set { SetValue(TextColorProperty, value); }
         }
 
-        public static readonly BindableProperty CloseButtonBackGroundColorProperty = BindableProperty.Create("CloseButtonBackGroundColor", typeof(Color), typeof(SnackBar), Color.Transparent);
-        public Color CloseButtonBackGroundColor
+        public static readonly BindableProperty ButtonBackGroundColorProperty = BindableProperty.Create(nameof(ButtonBackGroundColor), typeof(Color), typeof(SnackBar), Color.Transparent);
+        public Color ButtonBackGroundColor
         {
-            get { return (Color)GetValue(CloseButtonBackGroundColorProperty); }
-            set { SetValue(CloseButtonBackGroundColorProperty, value); }
+            get =>  (Color)GetValue(ButtonBackGroundColorProperty); 
+            set { SetValue(ButtonBackGroundColorProperty, value); }
         }
 
-        public static readonly BindableProperty FontFamilyProperty = BindableProperty.Create("FontFamily", typeof(string), typeof(SnackBar), default(string));
+        public static readonly BindableProperty FontFamilyProperty = BindableProperty.Create(nameof(FontFamily), typeof(string), typeof(SnackBar), default(string));
         public string FontFamily
         {
-            get { return (string)GetValue(FontFamilyProperty); }
+            get =>  (string)GetValue(FontFamilyProperty); 
             set { SetValue(FontFamilyProperty, value); }
         }
 
@@ -77,17 +92,17 @@ namespace Xamarin.FormsSnackBarDemo
             set { SetValue(ButtonCommandProperty, value); }
         }
 
-        static void OnButtonCommandPropertyChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            if (bindable is SnackBar snackBar && newValue is ICommand command)
-            {
-                snackBar.ButtonCommand = command;
-            }
-        }
+        //static void OnButtonCommandPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        //{
+        //    if (bindable is SnackBar snackBar && newValue is ICommand command)
+        //    {
+        //        snackBar.ButtonCommand = command;
+        //    }
+        //}
         #endregion
 
-
         #region Properties
+
         public uint AnimationDuration { get; set; } = 200;
 
         public uint TimeoutDuration { get; set; }
@@ -108,7 +123,7 @@ namespace Xamarin.FormsSnackBarDemo
         public async void Close()
         {
             Message = string.Empty;
-            await this.TranslateTo(0, 50, AnimationDuration);
+            await this.TranslateTo(0, HeightRequest, AnimationDuration);
         }
 
         public async void Open(string message)
